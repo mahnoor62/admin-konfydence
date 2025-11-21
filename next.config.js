e3816ec291b72a147562449049ddb6e31053c719
mahnoor62 <148554953+mahnoor62@users.noreply.github.com>
2025-11-21 17:15:45 +0500

    c
    c

--- next.config.js
+++ next.config.js
@@ -1,21 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
-        pathname: '/uploads/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost',
+//         port: '5000',
+//         pathname: '/uploads/**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: '**',
+//       },
+//     ],
+//   },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  reactStrictMode: false,
+  // output: 'export',
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+};
 
